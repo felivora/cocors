9efa1082b6e756dfd330bcf253a0e3505e530c6d
Felix Krueger <mail@kruegerfelix.de>
2022-07-22 21:06:25 +0200

    feat(coco): Finished the Ord implementation for Version

--- coco/src/semantic_version/version.rs
+++ coco/src/semantic_version/version.rs
@@ -2,6 +2,7 @@
 
 use regex::Regex;
 use std::fmt;
+use std::cmp::Ordering;
 
 use crate::{CommitType, ConventionalCommit};
 
@@ -68,15 +69,13 @@ impl Version {
     /// ```
     pub fn parse(version: &str) -> Option<Version> {
         let version_regex =
-            Regex::new(r"(\d+)\.(\d+)\.(\d+)(-[0-9A-Za-z-]+)?(\+[0-9A-Za-z-]+)?").unwrap();
+            Regex::new(r"(\d+)\.(\d+)\.(\d+)(-[0-9A-Za-z-\.]+)?(\+[0-9A-Za-z-\.]+)?").unwrap();
 
         let caps_option = version_regex.captures(version);
 
         // return early if the regex did not find anything
-        if caps_option.is_none() {
-            return None;
-        }
-
+        caps_option.as_ref()?;
+        
         let caps = caps_option.unwrap();
 
         let major = caps.get(1).map(|m| m.as_str());
@@ -103,8 +102,8 @@ impl Version {
             major: 0,
             minor: 0,
             patch: 0,
-            pre_release: pre_release,
-            metadata: metadata,
+            pre_release,
+            metadata,
         };
 
         match major.unwrap().parse::<u64>() {
@@ -125,6 +124,9 @@ impl Version {
         Some(semver)
     }
 
+    /// Increments the version according to the [conventional commit specification](https://www.conventionalcommits.org/en/v1.0.0/#specification)
+    ///
+    /// Using a human readable format
     pub fn bump(&mut self, commit: &ConventionalCommit) {
         if commit.breaking {
             self.major += 1;
@@ -159,7 +161,7 @@ impl Version {
             CommitType::Fix => self.patch -= 1,
             CommitType::Feature => self.minor -= 1,
             CommitType::BreakingChange => self.major -= 1,
-            _ => return,
+            _ => (),
         }
     }
 }
@@ -174,12 +176,163 @@ impl fmt::Display for Version {
             self.patch,
             self.pre_release
                 .as_ref()
-                .map_or_else(|| String::new(), |p| { format!("-{}", p) }),
+                .map_or_else(String::new, |p| { format!("-{}", p) }),
             self.metadata
                 .as_ref()
-                .map_or_else(|| String::new(), |p| { format!("+{}", p) })
+                .map_or_else(String::new, |p| { format!("+{}", p) })
         )
     }
+}
+
+impl Ord for Version{
+    fn cmp(&self, other: &Self) -> Ordering {
+        
+        if self.major != other.major {
+            return self.major.cmp(&other.major)
+        }
+
+        if self.minor != other.minor {
+            return self.minor.cmp(&other.minor)
+        }
+
+        if self.patch != other.patch {
+            return self.patch.cmp(&other.patch)
+        }
+        
+        if !(self.pre_release.is_some() && other.pre_release.is_some()){
+           return self.pre_release.cmp(&other.pre_release).reverse()
+        }
+       
+
+        // Pre Release precedence is calculated as follows:
+        // 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta < 1.0.0-beta.2 <
+        // 1.0.0-beta.11 < 1.0.0-rc.1 < 1.0.0
+        // Option was checked above (returned only if not both are some)
+        // so unwrapping here is fine 
+        let self_prerelease_clone = self.pre_release.clone().unwrap();
+        let mut self_prerelease = self_prerelease_clone.split('.');
+        let other_prerelease_clone = other.pre_release.clone().unwrap();
+        let mut other_prerelease = other_prerelease_clone.split('.');
+      
+        loop {
+           let s = self_prerelease.next();
+           let o = other_prerelease.next();
+           
+           if s.is_none() && o.is_none(){
+                break;
+           }; 
+
+           if s.is_none() {
+               return Ordering::Less;
+           }
+
+           if o.is_none() {
+               return Ordering::Greater;
+           }
+
+           if s.cmp(&o) != Ordering::Equal{
+               return s.cmp(&o)
+           }
+        }
+       
+        Ordering::Equal
+        
+    }
+}
+
+impl PartialOrd for Version {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+
+#[cfg(test)]
+mod ordering_test {
+
+    use crate::Version;
+    use std::cmp::Ordering;
+    
+    #[test]
+    fn major_version_diff() {
+        let version_greater = Version::parse("2.2.3").unwrap();
+        let version_less = Version::parse("1.2.3").unwrap();
+
+        assert_eq!(version_greater.cmp(&version_less), Ordering::Greater);
+    }
+   
+    #[test]
+    fn minor_version_diff() {
+        let version_greater = Version::parse("1.3.3").unwrap();
+        let version_less = Version::parse("1.2.3").unwrap();
+
+        assert_eq!(version_greater.cmp(&version_less), Ordering::Greater);
+    }
+ 
+    #[test]
+    fn patch_version_diff() {
+        let version_greater = Version::parse("1.2.4").unwrap();
+        let version_less = Version::parse("1.2.3").unwrap();
+
+        assert_eq!(version_greater.cmp(&version_less), Ordering::Greater);
+    }
+
+    #[test]
+    fn one_with_pre_release_diff() {
+        let version_greater = Version::parse("1.2.3").unwrap();
+        let version_less = Version::parse("1.2.3-alpha").unwrap();
+
+        assert_eq!(version_greater.cmp(&version_less), Ordering::Greater);
+    }
+
+    #[test]
+    fn pre_release_equal() {
+        let version_greater = Version::parse("1.2.3-alpha").unwrap();
+        let version_less = Version::parse("1.2.3-alpha").unwrap();
+
+        assert_eq!(version_greater.cmp(&version_less), Ordering::Equal);
+    }
+    
+    #[test]
+    fn pre_release_same_amount_alpha_diff() {
+        let version_greater = Version::parse("1.2.3-beta").unwrap();
+        let version_less = Version::parse("1.2.3-alpha").unwrap();
+
+        assert_eq!(version_greater.cmp(&version_less), Ordering::Greater);
+    }
+    
+    #[test]
+    fn pre_release_same_amount_numeric_diff() {
+        let version_greater = Version::parse("1.2.3-alpha.beta").unwrap();
+        let version_less = Version::parse("1.2.3-alpha.1111").unwrap();
+
+        assert_eq!(version_greater.cmp(&version_less), Ordering::Greater);
+    }
+
+    #[test]
+    fn pre_release_different_amount() {
+        let version_greater = Version::parse("1.2.3-alpha.1").unwrap();
+        let version_less = Version::parse("1.2.3-alpha").unwrap();
+
+        assert_eq!(version_greater.cmp(&version_less), Ordering::Greater);
+    }
+
+    #[test]
+    fn pre_release_difference_exists() {
+        let version_greater = Version::parse("1.2.3").unwrap();
+        let version_less = Version::parse("1.2.3-alpha.1").unwrap();
+
+        assert_eq!(version_greater.cmp(&version_less), Ordering::Greater);
+    }
+
+    #[test]
+    fn pre_release_sanity_check() {
+        let version_greater = Version::parse("1.2.3-beta.11").unwrap();
+        let version_less = Version::parse("1.2.3-alpha.1").unwrap();
+
+        assert_eq!(version_greater.cmp(&version_less), Ordering::Greater);
+    }
+
 }
 
 #[cfg(test)]
@@ -206,10 +359,10 @@ mod format_test {
             major: 1,
             minor: 2,
             patch: 3,
-            pre_release: Some(String::from("alpha")),
+            pre_release: Some(String::from("alpha.beta.3")),
             metadata: Some(String::from("d408340")),
         };
-        assert_eq!(format!("{version}"), "1.2.3-alpha+d408340");
+        assert_eq!(format!("{version}"), "1.2.3-alpha.beta.3+d408340");
     }
 
     #[test]
@@ -248,11 +401,11 @@ mod parse_test {
             major: 1,
             minor: 2,
             patch: 3,
-            pre_release: Some(String::from("alpha")),
+            pre_release: Some(String::from("alpha.beta.3")),
             metadata: None,
         };
 
-        assert_eq!(Version::parse("1.2.3-alpha"), Some(version));
+        assert_eq!(Version::parse("1.2.3-alpha.beta.3"), Some(version));
     }
 
     #[test]
